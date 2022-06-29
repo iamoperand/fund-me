f6edc04d35f381678f67ea03834c08978e25ea6c
nikhil arora <narora200@gmail.com>
2022-06-29 18:24:37 +0530

    refactor: according to solidity style guide

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,8 +4,14 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
 
+/**
+ * @title A contract for crowd funding
+ * @author Nikhil
+ * @notice This contract is to demo a sample funding contract
+ * @dev This implements price feeds as our library
+ */
 contract FundMe {
   using PriceConverter for uint256;
 
@@ -17,9 +23,24 @@ contract FundMe {
 
   AggregatorV3Interface public priceFeed;
 
+  modifier onlyOwner() {
+    if (msg.sender != iOwner) {
+      revert FundMe__NotOwner();
+    }
+    _;
+  }
+
   constructor(address priceFeedAddress) {
     iOwner = msg.sender;
     priceFeed = AggregatorV3Interface(priceFeedAddress);
+  }
+
+  receive() external payable {
+    fund();
+  }
+
+  fallback() external payable {
+    fund();
   }
 
   function fund() public payable {
@@ -29,13 +50,6 @@ contract FundMe {
     );
     addressToAmountFunded[msg.sender] += msg.value;
     funders.push(msg.sender);
-  }
-
-  modifier onlyOwner() {
-    if (msg.sender != iOwner) {
-      revert NotOwner();
-    }
-    _;
   }
 
   function withdraw() public payable onlyOwner {
@@ -49,13 +63,5 @@ contract FundMe {
       value: address(this).balance
     }("");
     require(callSuccess, "call failed!");
-  }
-
-  fallback() external payable {
-    fund();
-  }
-
-  receive() external payable {
-    fund();
   }
 }
